dddf345afc9ca0b25dd156c843c78603041bafb0
Fedyashev Andrei <fedjashev@gmail.com>
2019-02-26 00:28:02 +0300

    First commit

---
+++ src/js/index.js
@@ -0,0 +1,208 @@
+'use strict'
+
+class AudioAnalizer {
+
+    constructor() {
+        this.start = this.start.bind(this);
+        this.stop = this.stop.bind(this);
+        this.stopWithCallback = this.stopWithCallback.bind(this);
+        this.play = this.play.bind(this);
+        this.clear = this.clear.bind(this);
+        Promise.resolve().then(async () => {
+
+            console.log('Promise resolve begin');
+
+            this.state = {
+                mediaRecorder: null,
+                audioChunks: [],
+                audioBlob: null,
+                audioUrl: null,
+                buffer: [],
+                getBufferPromise: null
+            }
+
+            const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+            this.state.mediaRecorder = new MediaRecorder(stream);
+
+            this.state.mediaRecorder.addEventListener('dataavailable', e => {
+                this.state.audioChunks.push(e.data);
+            });
+
+            // this.state.mediaRecorder.addEventListener('stop', e => {
+            //     this.state.audioBlob = new Blob(this.state.audioChunks);
+            //     this.state.audioUrl = URL.createObjectURL(this.state.audioBlob);
+            // });
+        });
+    }
+
+    start() {
+        console.log('start');
+        this.state.mediaRecorder.start();
+    }
+
+    stop() {
+        console.log('stop');
+        this.state.mediaRecorder.addEventListener('stop', e => {
+            this.state.audioBlob = new Blob(this.state.audioChunks);
+            this.state.audioUrl = URL.createObjectURL(this.state.audioBlob);
+
+            //////////
+
+            const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+            const source = audioCtx.createBufferSource();
+
+            let fileReader = new FileReader();
+
+            fileReader.onloadend = () => {
+                audioCtx.decodeAudioData(fileReader.result,
+                    buffer => {
+                        source.buffer = buffer;
+                        //this.state.buffer = buffer;
+                        //this.state.getBufferPromise = Promise.resolve({buffer});
+
+                        //console.log(buffer);
+
+                        source.connect(audioCtx.destination);
+                        source.start(0);
+                    },
+                    err => {
+                        console.log(err);
+                    }
+                );
+            }
+
+            fileReader.readAsArrayBuffer(this.state.audioBlob);
+        });
+
+        this.state.mediaRecorder.stop();
+    }
+
+    stopWithCallback = (callback) => {
+        console.log('stop async');
+
+        this.state.mediaRecorder.addEventListener('stop', e => {
+            this.state.audioBlob = new Blob(this.state.audioChunks);
+            this.state.audioUrl = URL.createObjectURL(this.state.audioBlob);
+
+            const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+            let fileReader = new FileReader();
+            fileReader.onloadend = () => {
+                audioCtx
+                    .decodeAudioData(fileReader.result)
+                    .then(buffer => {
+                        this.state.buffer = buffer;
+                        callback(buffer);
+                    })
+                    .catch(err => console.log(err));
+            }
+            fileReader.readAsArrayBuffer(this.state.audioBlob);
+        });
+
+        this.state.mediaRecorder.stop();
+    }
+
+    play() {
+        console.log('play');
+        const audio = new Audio(this.state.audioUrl);
+        audio.play();
+        //console.log(audio);
+    }
+
+    clear() {
+        this.state.audioUrl = null;
+        this.state.audioBlob = null;
+        this.state.audioChunks = [];
+        this.state.buffer = [];
+    }
+
+}
+
+// CANVAS
+const canvasWidth = window.innerWidth, canvasHeight = 120;
+const newCanvas = createCanvas(canvasWidth, canvasHeight);
+const context = newCanvas.getContext('2d');
+
+window.onload = () => {
+
+    document.getElementById('canvas-container').appendChild(newCanvas);
+    //document.body.appendChild(newCanvas);
+
+    let analizer = new AudioAnalizer();
+
+    alert("Let's go!");
+
+    const btnStart = document.getElementById('btn-start');
+    const btnStop = document.getElementById('btn-stop');
+    const btnPlay = document.getElementById('btn-play');
+    const btnClear = document.getElementById('btn-clear');
+
+    btnStart.onclick = e => {
+        e.preventDefault();
+        console.log('btn-start');
+        console.log(analizer);
+        analizer.start();
+    }
+
+    btnStop.onclick = async (e) => {
+        e.preventDefault();
+        console.log('btn-stop');
+        analizer.stopWithCallback(buffer => {
+            console.log(buffer);
+            displayBuffer(buffer);
+            const leftChannel = buffer.getChannelData(0);
+            let max = leftChannel[0];
+            leftChannel.forEach(item => {
+                max = item > max ? item : max
+            });
+            document.getElementById('max-amplitude').innerText = max;
+        });
+    }
+
+    btnPlay.onclick = e => {
+        e.preventDefault();
+        console.log('btn-play');
+        analizer.play();
+    }
+
+    btnClear.onclick = e => {
+        e.preventDefault();
+        console.log('btn-clear');
+        analizer.clear();
+    }
+};
+
+function displayBuffer(buff /* is an AudioBuffer */) {
+    const drawLines = 500;
+    const leftChannel = buff.getChannelData(0); // Float32Array describing left channel     
+    const lineOpacity = canvasWidth / leftChannel.length;
+    context.save();
+    context.fillStyle = '#080808';
+    context.fillRect(0, 0, canvasWidth, canvasHeight);
+    context.strokeStyle = '#46a0ba';
+    context.globalCompositeOperation = 'lighter';
+    context.translate(0, canvasHeight / 2);
+    //context.globalAlpha = 0.6 ; // lineOpacity ;
+    context.lineWidth = 1;
+    const totallength = leftChannel.length;
+    const eachBlock = Math.floor(totallength / drawLines);
+    const lineGap = (canvasWidth / drawLines);
+
+    context.beginPath();
+    for (let i = 0; i <= drawLines; i++) {
+        const audioBuffKey = Math.floor(eachBlock * i);
+        const x = i * lineGap;
+        const y = leftChannel[audioBuffKey] * canvasHeight / 2;
+        context.moveTo(x, y);
+        context.lineTo(x, (y * -1));
+    }
+    context.stroke();
+    context.restore();
+}
+
+function createCanvas(w, h) {
+    const newCanvas = document.createElement('canvas');
+    newCanvas.width = w;
+    newCanvas.height = h;
+    return newCanvas;
+};
